93254a29d719b12718abc7f05ddb1313a363dca0
mahdee18 <mahdeec10@gmail.com>
2023-06-11 05:40:18 +0600

    Get Data for Instructor

--- index.js
+++ index.js
@@ -105,7 +105,32 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.send(result);
         });
+        // Role Instructor
 
+        app.get("/users/instructor/:email", verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded.email !== email) {
+                res.send({ instructor: false })
+            }
+
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const result = { instructor: user?.role === "instructor" }
+            res.send(result)
+        })
+
+
+        app.patch("/users/instructor/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: "instructor",
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
 
         
 
