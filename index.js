7178f5e337f1b6e03fe6b86086426e62fd1e317f
mahdee18 <mahdeec10@gmail.com>
2024-02-18 18:34:00 +0600

    Updated

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const verifyJWT = (req, res, next) => {
     if (!authorization) {
         return res.status(401).send({ error: true, message: 'Unauthorized Access!' })
     }
+    
     const token = authorization.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
         if (error) {
